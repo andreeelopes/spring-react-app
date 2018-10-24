ca61a1b7e8d7f3d2384837344c2804baa6566002
SimaoD <s.dolores@campus.fct.unl.pt>
2018-10-24 16:17:42 +0100

    initializing server project

---
+++ server/src/main/java/pt/unl/fct/ecma/EcmaApplication.java
@@ -0,0 +1,12 @@
+package pt.unl.fct.ecma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcmaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcmaApplication.class, args);
+    }
+}
