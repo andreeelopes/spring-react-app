996a397eafa16800d17ce995a398c503cc875495
SimaoD <s.dolores@campus.fct.unl.pt>
2018-11-04 17:46:24 +0000

    isPrincipal to IsPrincipal

--- server/src/main/java/pt/unl/fct/ecma/security/IsPrincipal.java
+++ server/src/main/java/pt/unl/fct/ecma/security/IsPrincipal.java
@@ -8,7 +8,7 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @Documented
-@PreAuthorize(isPrincipal.Condition)
-public @interface isPrincipal {
+@PreAuthorize(IsPrincipal.Condition)
+public @interface IsPrincipal {
     String Condition = "@mySecurityService.isPrincipal(principal, #id)";
 }
