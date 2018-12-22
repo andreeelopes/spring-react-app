9eda41abda18190a3afd6d109700b75e1c412a43
Nelson Coquenim <n.coquenim@campus.fct.unl.pt>
2018-12-22 12:44:28 +0000

    Added a new Icon for the maximize of the review details

--- client/react-redux-tsx/src/components/proposals/reviews/ReviewLine.tsx
+++ client/react-redux-tsx/src/components/proposals/reviews/ReviewLine.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 import {Link} from "react-router-dom";
 
-import {Button} from "react-bootstrap";
 import {connect} from "react-redux";
 import {showModal} from "../../../actions/reviews/reviewModalAction";
 
@@ -18,7 +17,7 @@ export class ReviewLine extends React.Component<any> {
             >
                 <Link
                     to={'/proposals/' + this.props.review.proposal.id}>{(this.props.section) ? this.props.section.text : null}</Link> {this.props.review.score}
-                <Button onClick={() => this.open(this.props.review)}>Open</Button>
+                <span onClick={() => this.open(this.props.review)}className="review-popup glyphicon glyphicon-fullscreen" aria-hidden="true"/>
             </div>
         );
     }
