dc71d8e9f16910d738dc23a6ba56bb1cc1633f9b
SimaoD <s.dolores@campus.fct.unl.pt>
2018-12-22 16:26:16 +0000

    Fix add comment

--- client/react-redux-tsx/src/actions/comments/commentFormAction.ts
+++ client/react-redux-tsx/src/actions/comments/commentFormAction.ts
@@ -1,10 +1,14 @@
 import Axios from "axios";
 import {ADD_PROPOSAL_COMMENT} from "../proposals/types";
+import {getUser} from "../getSessionUser";
 
 export const submitComment = (userId: number, proposal: string, text: string) => (dispatch: any) => {
+
     const json = {author: {id: userId.toString()}, proposal: {id: proposal}, comment: text};
     return Axios.post("http://localhost:8080/proposals/" + proposal + "/comments/", json,
         {withCredentials: true}).then(() => {
+            const user= getUser();
+            json.author=user;
         dispatch({
             type: ADD_PROPOSAL_COMMENT,
             payload: json
